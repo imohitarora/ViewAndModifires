3380df9dcdaa1e99bb97584405a33354d3c6849f
Mohit Arora <me@aboutmohit.com>
2024-04-21 20:05:37 -0400

    Initial Commit

---
+++ ViewAndModifires/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewAndModifires
+//
+//  Created by Mohit Arora on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewAndModifires/ViewAndModifiresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiresApp.swift
+//  ViewAndModifires
+//
+//  Created by Mohit Arora on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
